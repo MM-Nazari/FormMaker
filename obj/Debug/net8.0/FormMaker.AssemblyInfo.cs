063c028a880d782d6eec41c51b9a1d0eac8c4762
M.Nazari <nazarimohammadmehdi6@gmail.com>
2024-12-23 15:05:23 +0330

    Migrations Added

--- obj/Debug/net8.0/FormMaker.AssemblyInfo.cs
+++ obj/Debug/net8.0/FormMaker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FormMaker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c60d1d8865b1e27e791dac0f4831a1be932aeae5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b74f0cd22e55a5e79dc1c33c2d7c54edd714aae")]
 [assembly: System.Reflection.AssemblyProductAttribute("FormMaker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FormMaker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
